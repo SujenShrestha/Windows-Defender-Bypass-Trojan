dcded56ff19eb7b13c58b0dd99febc34c23b76a2
SujenShrestha <88645443+SujenShrestha@users.noreply.github.com>
2023-01-01 17:44:41 +0545

    Update trojan.cs

--- trojan.cs
+++ trojan.cs
@@ -1,14 +1,3 @@
-/* SIMPLE PROCESS INJECTION
-description: |
-	Injects shellcode into an a newly spawned remote process.
-key win32 API calls:
-  - kernel32.dll:
-    1: 'OpenProcess'
-    2: 'VirtualAllocEx (PAGE_EXECUTE_READ_WRITE)'
-    3: 'WriteProcessMemory'
-    4: 'CreateRemoteThread'
-*/
-
 using System;
 using System.Diagnostics;
 using System.Reflection;
